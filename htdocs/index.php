4f82de0e3c6ff18dec180ec0755f23b09bef09d4
Edwin Siebel <edwin@yard.nl>
2017-12-28 16:00:19 +0100

    Add: docker setup

---
+++ htdocs/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Docker <?php echo 'test' ?></title>
+    </head>
+    <body>
+        <h1>Docker <?php echo 'test2' ?></h1>
+    </body>
+</html>
